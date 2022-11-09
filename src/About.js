02a6750169b2b2dfcf2d36c14c3e9f015dd473dc
Minhvan Le <mple330@gmail.com>
2022-11-09 02:39:19 -0800

    submit functionality done

--- src/About.js
+++ src/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
     return (
-        <main>About</main>
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This blog app is a project in the Learn React tutorial series.</p>
+        </main>
     )
 }
 
--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
   // useNavigate
 } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -47,10 +48,24 @@ function App() {
   const [postBody, setPostBody] = useState('');
   // const navigate = useNavigate();
 
+  useEffect(() => {
+    const filteredResults = posts.filter(post =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search])
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    // navigate('/');
   }
 
   const handleDelete = (id) => {
@@ -70,12 +85,12 @@ function App() {
         />
         <Routes>
           <Route path="/" element={<Home
-            posts={posts}
+            posts={searchResults}
           />} />
           <Route path="/post" element={<NewPost
             postTitle={postTitle}
             setPostTitle={setPostTitle}
-            postBody={setPostBody}
+            postBody={postBody}
             setPostBody={setPostBody}
             handleSubmit={handleSubmit}
           />} />
