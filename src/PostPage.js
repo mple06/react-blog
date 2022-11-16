99f9adf08e104db96c8d96a0feb1a6406f5b281d
Minhvan Le <mple330@gmail.com>
2022-11-16 00:24:21 -0800

    added axios and edit post functionality

--- src/PostPage.js
+++ src/PostPage.js
@@ -13,7 +13,8 @@ const PostPage = ({ posts, handleDelete }) => {
                         <h2>{post.title}</h2>
                         <p className="postDate">{post.datetime}</p>
                         <p className="postBody">{post.body}</p>
-                        <button onClick={() => handleDelete(post.id)}>
+                        <Link to={`/edit/${post.id}`}><button className="editButton">Edit Post</button></Link>
+                        <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                             Delete Post
                         </button>
                     </>
@@ -27,7 +28,6 @@ const PostPage = ({ posts, handleDelete }) => {
                         </p>
                     </>
                 }
-
             </article>
         </main>
     )
